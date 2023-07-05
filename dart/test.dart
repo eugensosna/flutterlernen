2fea06b633a644652fff47213e1e146dad818de8
Eugen Sosna <eugensosna@gmx.de>
2023-07-05 22:10:39 +0200

    jj

--- dart/test.dart
+++ dart/test.dart
@@ -1,5 +1,24 @@
+class Greeter {
+  var greeting;
+  var _name;
+
+  sayHello() {
+    return "$greeting ${this.name}";
+  }
+
+  get name => _name;
+  set name(value) => _name = value;
+}
+
 void main() {
   var hello = "HEllo";
   final w = "World";
   print('$hello $w');
+  print(r'$hello $w'); //one params
+  print("${hello.toUpperCase()}");
+  Greeter greeter = new Greeter();
+  greeter.greeting = "Hello";
+  greeter.name = "World";
+
+  print(greeter.sayHello());
 }
