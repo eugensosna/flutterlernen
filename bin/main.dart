5e351431c7135b06edd601a96438f05811f803a4
Eugen Sosna <eugensosna@gmx.de>
2023-07-11 21:47:06 +0200

    simple types

--- bin/main.dart
+++ bin/main.dart
@@ -28,4 +28,24 @@ void main(List<String> args) {
   var gree = Greeteng();
   gree.name = "bob";
   sayHallo(gree);
+  // String -> int
+  var myInt = int.parse('34'); // string to int
+  // String -> double
+  var myDouble = double.parse('11.45');
+  // int -> String
+  String s1 = 14.toString();
+  String s2 = myInt.toString();
+  // double -> String
+  String s3 = 3.14159.toStringAsFixed(2); // 2 числа после точки
+  String s4 = myDouble.toString();
+  print(s3);
+  print(s4);
+  var mySet = <int>{1, 2, 5, 5, 5, 6, 7, 8};
+  print((mySet));
+  var myList = [
+    [10, 3, 4, 1]
+  ];
+  var newList = List.from(myList);
+  newList[0].add(77);
+  print('elements myList: $myList');
 }
